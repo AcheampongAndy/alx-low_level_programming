11c0c957860437a6ee0fbc5581a97b92f6c2d945
AcheampongAndy <acheampongandrews1999@gmail.com>
2022-12-13 10:38:57 -0800

    the first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,15 @@
 int main(void)
 {
 	long int total, first, second;
+
 	int num;
 
-	total =0;
-	first = 1;
-	second = 2;
+	total = 0;
+	first = 0;
+	second = 1;
 	num = 0;
 
-	while (num < 50)
+	while (num < 49)
 	{
 		total = first + second;
 		printf("%li, ", total);
