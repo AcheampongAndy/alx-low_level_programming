ad894e2ed98004cd2a65686a3097be5e923fcd42
AcheampongAndy <acheampongandrews1999@gmail.com>
2022-12-15 01:52:22 -0800

    prints the numbers, from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
+
