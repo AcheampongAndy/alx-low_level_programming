9a371312b1d7402358a3853d0f777b2e8a6a39d8
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-01-10 17:41:19 -0800

    concatenates all the arguments

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * len - gets string length
+ * @s: string
+ * Return: length (int)
+ */
+
+int _len(char *s)
+{
+	int l;
+
+	for (l = 0; string[l] != '\0'; l++)
+		;
+
+	return (l);
+}
+/**
+ * argstostr - concatenates all the arguments
+ * @ac: argc - number of arguments
+ * @av: argv - array of arguments
+ * Return: pointe to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int l, x, y, k;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	l = 0;
+	for (x = 0; x < ac; x++)
+		l += _len(av[x]) + 1;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+
+	k = 0;
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; y < _len(av[x]); y++)
+		{
+			str[k] = av[x][y];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
+}
