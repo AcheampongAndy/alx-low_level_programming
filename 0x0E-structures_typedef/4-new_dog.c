d9222813f13d65c5fe54ae1136ed858043a2065b
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-01-17 05:53:50 -0800

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* newStr - to return a new string
+* @strg: the string
+* Return: a pointer
+*/
+char newStr(*strg)
+{
+	int x, y;
+
+	char *str
+
+	if (strg == NULL)
+		return (NULL);
+
+	for (x = 0; strg[x] != '\0'; x++)
+		;
+
+	str = malloc(sizeof(char) * x + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (y = 0; strg[y] != '\0'; y++)
+		str[y] = strg[y];
+	str[y] = '\0';
+
+	return (str);
+}
+/**
+* new_dog - creates a new dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Return: new_dog (dog_t)
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	char *dname, downer;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dname = newStr(name);
+	if (dname == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	downer = newStr(downer);
+	if (downer == NULL)
+	{
+		free(dog);
+		free(dname);
+		return (NULL);
+	}
+
+	dog.name = dname;
+	dog.age = age;
+	dog.owner = downer;
+	return (dog);
+}
