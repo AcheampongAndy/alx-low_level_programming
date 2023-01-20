5b74d3fd4fdd6c588b994a58233edcc6f11278f7
AcheampongAndy <acheampongandrews1999@gmail.com>
2023-01-19 16:04:39 -0800

    sum of all its parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list arg;
 	int add;
 	unsigned int x;
 
--- 0x10-variadic_functions/_putchar.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int _putchar(char c)
-{
-	int a;
-
-	a = putchar(c);
-	return (a);
-}
